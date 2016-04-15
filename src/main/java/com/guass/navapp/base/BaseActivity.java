8f04e9bf93a09bbd5cc82940f48660935ebe5bea
guass <624512733@qq.com>
2016-04-15 09:25:27 +0800

    update

--- src/main/java/com/guass/navapp/base/BaseActivity.java
+++ src/main/java/com/guass/navapp/base/BaseActivity.java
@@ -3,6 +3,8 @@ package com.guass.navapp.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by guass on 2016/3/14.
@@ -11,6 +13,13 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window window = getWindow();
+
+//        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+//                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         initView();
         initActionBar();
     }
